2d78475d29b6fc52d0cfbc21db0bd6e811f157e3
DogukanBaygut <hulavci121@gmail.com>
2022-12-21 18:36:50 +0300

    Update v1.1
    Lol Auto Champ Selector

---
+++ league_auto_accept.py
@@ -0,0 +1,92 @@
+import pyautogui
+import colorama
+import termcolor
+from colorama import init, Back, Fore, Style
+from time import sleep
+global accepted
+global banned
+global chose
+accepted = False
+banned = False
+chose = False
+
+def findimage(image):
+    cords = pyautogui.locateOnScreen(image,confidence=0.98)
+    if(cords is not None):
+        return cords
+
+def click(img):
+    sleep(2)
+    if(findimage(img) is not None):
+        pyautogui.click(findimage(img))
+def main():
+    colorama.init() 
+    print(Fore.GREEN + "█░░ █▀▀█ █░░   █▀▀ █░░█ █▀▀█ █▀▄▀█ █▀▀█   █▀▀ █▀▀ █░░ █▀▀ █▀▀ ▀▀█▀▀ █▀▀█ █▀▀█ ")
+    print(Fore.GREEN + "█░░ █░░█ █░░   █░░ █▀▀█ █▄▄█ █░▀░█ █░░█   ▀▀█ █▀▀ █░░ █▀▀ █░░ ░░█░░ █░░█ █▄▄▀ ")
+    print(Fore.GREEN + "▀▀▀ ▀▀▀▀ ▀▀▀   ▀▀▀ ▀░░▀ ▀░░▀ ▀░░░▀ █▀▀▀   ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ░░▀░░ ▀▀▀▀ ▀░▀▀ ")
+    print(Back.WHITE + "█▀▄▀█ █▀▀█ █▀▀▄ █▀▀   █▀▀▄ █░░█   █▀▀▄ █▀▀█ █▀▀▄ █▀▀█┓")
+    print(Fore.GREEN + "█░▀░█ █▄▄█ █░░█ █▀▀   █▀▀▄ █▄▄█   █░░█ █░░█ █░░█ █░░█┃ ")
+    print(Fore.GREEN + "▀░░░▀ ▀░░▀ ▀▀▀░ ▀▀▀   ▀▀▀░ ▄▄▄█   ▀▀▀░ ▀▀▀▀ ▀▀▀░ ▀▀▀▀┃ ")
+    print(Back.RESET + "-------------------------------------------------------------------- ")
+    print(Back.RESET + "-------------------------------------------------------------------- ")
+    print(Fore.GREEN + "-------------------------------------------------------------------- ")
+    
+
+
+
+
+
+
+
+    
+
+
+    print(Fore.MAGENTA+ "https://github.com/DogukanBaygut")
+    print(Fore.CYAN + "By Dodo")
+    print(Fore.YELLOW + "V1.1.0")
+    print(Fore.WHITE + "---")
+    to_ban = input("what champion to ban?: ")
+    to_select= input("what champion to play?: ")
+    global accepted,banned,chose
+    print("[+] Started Searching [+]")
+    #keep searching for the accept button
+    while findimage("accepted.png") is None and accepted == False:
+        sleep(0.1)
+        try:
+            click("accept.png")
+        except:
+            print("cant find accept")
+    accepted = True
+    print("Game Started!")
+    while findimage("ban_search.png") is None and banned == False:
+        print(findimage("ban_search.png"))
+        sleep(0.1)
+    print("[+] Found Ban Button [+]")
+    ban_cords = findimage("ban_search.png")
+    click("ban_search.png")
+    print("[+] Clicked Ban Button [+]")
+    sleep(0.3)
+    pyautogui.write(to_ban,interval=0.25)
+    sleep(0.5)
+    pyautogui.click(ban_cords[0]-378,ban_cords[1]+55)
+    sleep(1)
+    click("ban_button.png")
+    sleep(0.5)
+    print("Banned "+ to_ban + "!")
+    banned = True
+    print("Waiting for champ select option")
+    while findimage("normal_search.png") is None and chose == False:
+        sleep(0.5)
+    normal_search_cords = findimage("normal_search.png")
+    print("Selecting!")
+    click("normal_search.png")
+    sleep(0.5)
+    pyautogui.write(to_select,interval=0.25)
+    sleep(1)
+    pyautogui.click(normal_search_cords[0]-378,normal_search_cords[1]+55)
+    sleep(1)
+    click("lock_button.png")
+    print("Selected [{0}] And Banned [{1}]".format(to_select,to_ban))
+    chose = True
+if __name__ == "__main__":
+    main()
